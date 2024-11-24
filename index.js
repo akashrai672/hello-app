6ce0e33d9b29a43aef491606b1799ba1f9158491
akashrai672 <68333218+akashrai672@users.noreply.github.com>
2024-11-24 16:59:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.send('Hello, World! me akash ra');
+    res.send('Hello, World! first deployement');
 });
 
 // Start the server
